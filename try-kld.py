ffe2034dee74aa9af9b8ea60beb09566de608f7d
Unknown <jaytin1218@hotmail.com>
2016-12-30 17:10:07 +0800

    mod: Add more conditions of KL test

--- try-kld.py
+++ try-kld.py
@@ -5,23 +5,27 @@ import numpy as np
 
 #initialize a normal distribution with frozen in mean=-1, std. dev.= 1
 blue = norm(loc = -1., scale = 1.0)
-red = norm(loc = 2., scale = 3.0)
+red = norm(loc = -1., scale = 3.0)
+black = norm(loc = 2., scale = 2.0)
+cyan0 = norm(loc = 5., scale = 1.0)
+cyan = norm(loc = 5., scale = 5.0)
 
 green = norm(loc = 0., scale = 2.0)
-cyan = norm(loc = 0., scale = 3.0)
 purple = norm(loc = 0., scale = 4.0)
 yellow = norm(loc = 0., scale = 5.0)
 
-x = np.arange(-10, 10, .1)
-y = np.arange(-10, 10, .1)
+x = np.arange(-20, 20, .1)
+y = np.arange(-20, 20, .1)
 
 #plot the pdfs of these normal distributions
 plt.plot(
-    # x, blue.pdf(x), 'b',
+    x, blue.pdf(x), 'b',
     x, green.pdf(x), 'g',
-    # x, red.pdf(x), 'r',
+    x, red.pdf(x), 'r',
     x, cyan.pdf(x), 'c',
     x, purple.pdf(x), 'm',
+    x, black.pdf(x), 'k',
+    x, cyan0.pdf(x), 'c',
     x, yellow.pdf(x), 'y'
 )
 
@@ -39,5 +43,12 @@ print 'Green to Yellow:', kl(green.pdf(x), yellow.pdf(x))
 print 'Yellow to Green:', kl(yellow.pdf(x), green.pdf(x))
 print 'Cyan to Yellow:', kl(cyan.pdf(x), yellow.pdf(x))
 print 'Yellow to Cyan:', kl(yellow.pdf(x), cyan.pdf(x))
+print ''
+print 'green to green:', kl(green.pdf(x), green.pdf(x))
+print 'green to cyan:', kl(green.pdf(x), cyan.pdf(x))
+print 'green to blue:', kl(green.pdf(x), blue.pdf(x))
+print 'green to red:', kl(green.pdf(x), red.pdf(x))
+print 'green to cyan0:', kl(green.pdf(x), cyan0.pdf(x))
+print 'green to black:', kl(green.pdf(x), black.pdf(x))
 
 plt.show()
