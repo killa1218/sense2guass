3b29ce146803d2addf1a0b21c1000378ea80bf2c
Unknown <jaytin1218@hotmail.com>
2017-02-20 19:25:38 +0800

    dev: Finish similarity test script

--- utils/testSim.py
+++ utils/testSim.py
@@ -10,7 +10,8 @@ sys.path.append(path.abspath(path.join(path.dirname(path.realpath(__file__)), pa
 from graph import batchSentenceLossGraph
 from e_step.inference import violentInference
 from vocab import Vocab
-from utils.distance import diagKL
+from utils.distance import diagKL as dist
+# from utils.distance import diagEL as dist
 from options import Options as opt
 
 
@@ -27,7 +28,7 @@ with tf.Session() as sess:
     minLossIdxGraph = tf.argmin(lossGraph, 0)
 
     sensePlaceholder = tf.placeholder(dtype=tf.int32)
-    distance = diagKL(tf.nn.embedding_lookup(vocab.means, sensePlaceholder[:, 0]), tf.nn.embedding_lookup(vocab.sigmas, sensePlaceholder[:, 0]), tf.nn.embedding_lookup(vocab.means, sensePlaceholder[:, 1]), tf.nn.embedding_lookup(vocab.sigmas, sensePlaceholder[:, 1]))
+    distance = dist(tf.nn.embedding_lookup(vocab.means, sensePlaceholder[:, 0]), tf.nn.embedding_lookup(vocab.sigmas, sensePlaceholder[:, 0]), tf.nn.embedding_lookup(vocab.means, sensePlaceholder[:, 1]), tf.nn.embedding_lookup(vocab.sigmas, sensePlaceholder[:, 1]))
     labelPlaceholder = tf.placeholder(dtype=tf.float64)
     difference = 10 - labelPlaceholder - distance
 
@@ -39,34 +40,54 @@ with tf.Session() as sess:
     for i in data:
         stcW = []
         word1 = i['w1']
-        w1sIdx = i['c1'].index(word1)
-        j = 1
+        if vocab.getWord(word1):
+            stcW.append(vocab.getWord(word1))
+            try:
+                w1sIdx = i['c1'].index(word1)
+            except:
+                print(i)
+            j = 1
 
-        while len(stcW) < opt.sentenceLength:
-            if w1sIdx - j >= 0 and vocab.getWord(i['c1'][w1sIdx - j]):
-                stcW.append(vocab.getWord(i['c1'][w1sIdx - j]))
-            if w1sIdx + j < len(i['c1']) and vocab.getWord(i['c1'][w1sIdx - j]):
-                stcW.append(vocab.getWord(i['c1'][w1sIdx + j]))
-        w1sIdx = stcW.index(word1)
+            while len(stcW) < opt.sentenceLength:
+                if len(stcW) < opt.sentenceLength and w1sIdx - j >= 0 and vocab.getWord(i['c1'][w1sIdx - j]):
+                    stcW.insert(0, vocab.getWord(i['c1'][w1sIdx - j]))
+                if len(stcW) < opt.sentenceLength and w1sIdx + j < len(i['c1']) and vocab.getWord(i['c1'][w1sIdx + j]):
+                    stcW.append(vocab.getWord(i['c1'][w1sIdx + j]))
+                j += 1
 
-        assign1 = violentInference(stcW, sess, minLossIdxGraph, placeholder)
+            for k in range(len(stcW)):
+                if stcW[k].token == word1:
+                    w1sIdx = k
+                    break
 
-        stcW = []
-        word2 = i['w2']
-        w2sIdx = i['c2'].index(word2)
-        j = 1
+            assign1 = violentInference(stcW, sess, minLossIdxGraph, placeholder)
 
-        while len(stcW) < opt.sentenceLength:
-            if w2sIdx - j >= 0 and vocab.getWord(i['c2'][w2sIdx - j]):
-                stcW.append(vocab.getWord(i['c2'][w2sIdx - j]))
-            if w2sIdx + j < len(i['c2']) and vocab.getWord(i['c2'][w2sIdx - j]):
-                stcW.append(vocab.getWord(i['c2'][w2sIdx + j]))
-        w2sIdx = stcW.index(word2)
+            stcW = []
+            word2 = i['w2']
+            if vocab.getWord(word2):
+                stcW.append(vocab.getWord(word2))
+                w2sIdx = i['c2'].index(word2)
+                j = 1
 
-        assign2 = violentInference(stcW, sess, minLossIdxGraph, placeholder)
+                while len(stcW) < opt.sentenceLength:
+                    if len(stcW) < opt.sentenceLength and w2sIdx - j >= 0 and vocab.getWord(i['c2'][w2sIdx - j]):
+                        stcW.append(vocab.getWord(i['c2'][w2sIdx - j]))
+                    if len(stcW) < opt.sentenceLength and w2sIdx + j < len(i['c2']) and vocab.getWord(i['c2'][w2sIdx + j]):
+                        stcW.append(vocab.getWord(i['c2'][w2sIdx + j]))
+                    j += 1
 
-        wordPairList.append([assign1[w1sIdx], assign2[w2sIdx]])
-        scoreList.append(i['r'])
+                for k in range(len(stcW)):
+                    if stcW[k].token == word2:
+                        w2sIdx = k
+                        break
 
-    print(sess.run(distance, feed_dict={sensePlaceholder: wordPairList}))
+                assign2 = violentInference(stcW, sess, minLossIdxGraph, placeholder)
+
+                wordPairList.append([assign1[w1sIdx], assign2[w2sIdx]])
+                scoreList.append(i['r'])
+
+    idx = sess.run(tf.argmin(distance, 0), feed_dict={sensePlaceholder: wordPairList})
+
+    print(idx)
+    print(data[idx]['r'])
 
