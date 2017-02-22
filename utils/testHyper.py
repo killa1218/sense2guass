35fab571807cddff64c8dfd1e76ef19a019771e8
Yiding TIan <jaytin1218@hotmail.com>
2017-02-22 09:58:20 +0800

    dev: Add hyper test script

--- utils/testHyper.py
+++ utils/testHyper.py
@@ -7,12 +7,9 @@ from os import path
 
 sys.path.append(path.abspath(path.join(path.dirname(path.realpath(__file__)), path.pardir)))
 
-from graph import batchSentenceLossGraph
-from e_step.inference import violentInference
 from vocab import Vocab
 from utils.distance import diagKL as dist
 # from utils.distance import diagEL as dist
-from options import Options as opt
 
 data = None
 vocab = None
@@ -26,22 +23,64 @@ with open('../data/BLESS/bless.pk3', 'rb') as f:
     vocab = Vocab()
     vocab.load('../data/vec.txt')
 
-with tf.Session() as sess:
+with tf.Session() as sess, open('../data/BLESS/result.mero.txt', 'w') as mf, open('../data/BLESS/result.hyper.txt', 'w') as hf, open('../data/BLESS/result.random.txt', 'w') as rf:
     # lossGraph, placeholder = batchSentenceLossGraph(vocab)
     # minLossIdxGraph = tf.argmin(lossGraph, 0)
 
     sensePlaceholder = tf.placeholder(dtype=tf.int32)
     distance = dist(tf.nn.embedding_lookup(vocab.means, sensePlaceholder[:, 0]), tf.nn.embedding_lookup(vocab.sigmas, sensePlaceholder[:, 0]), tf.nn.embedding_lookup(vocab.means, sensePlaceholder[:, 1]), tf.nn.embedding_lookup(vocab.sigmas, sensePlaceholder[:, 1]))
-    # labelPlaceholder = tf.placeholder(dtype=tf.float64)
-    # difference = 10 - labelPlaceholder - distance
+    reversedDistance = dist(tf.nn.embedding_lookup(vocab.means, sensePlaceholder[:, 1]), tf.nn.embedding_lookup(vocab.sigmas, sensePlaceholder[:, 1]), tf.nn.embedding_lookup(vocab.means, sensePlaceholder[:, 0]), tf.nn.embedding_lookup(vocab.sigmas, sensePlaceholder[:, 0]))
+    difference = distance - reversedDistance
+    minLossIdx = tf.argmin(distance, 0)
 
     tf.global_variables_initializer().run()
 
+    coveredWordNum = 0
+    # totalMeroNum = 0
+    # totalHyperNum = 0
+
     for i in data:
-        word = data['w']
-        if vocab.getWord(word):
-            for m in data['mero']:
-                if vocab.getWord(m):
-                    mero.append([])
+        word = vocab.getWord(i['w'])
+        if word:
+            coveredWordNum += 1
+            for m in i['mero']:
+                w = vocab.getWord(m)
+                if w:
+                    tmpList = []
+                    for k in range(word.senseStart, word.senseStart + word.senseNum):
+                        for j in range(w.senseStart, w.senseStart + w.senseNum):
+                            tmpList.append([k, j])
+
+                    distances, reversedDistances, minIdx, differences = sess.run([distance, reversedDistance, minLossIdx, difference], feed_dict={sensePlaceholder: tmpList})
+
+                    for k in range(len(distances)):
+                        mf.write('%s\t%s\t%.4f\t%.4f\n' % (word.token, w.token, distances[k], reversedDistances[k]))
+
+            for m in i['hyper']:
+                w = vocab.getWord(m)
+                if w:
+                    tmpList = []
+                    for k in range(word.senseStart, word.senseStart + word.senseNum):
+                        for j in range(w.senseStart, w.senseStart + w.senseNum):
+                            tmpList.append([k, j])
+
+                    distances, reversedDistances, minIdx, differences = sess.run([reversedDistance, distance, minLossIdx, difference], feed_dict={sensePlaceholder: tmpList})
+
+                    for k in range(len(distances)):
+                        hf.write('%s\t%s\t%.4f\t%.4f\n' % (w.token, word.token, distances[k], reversedDistances[k]))
+
+            for m in i['r']:
+                w = vocab.getWord(m)
+                if w:
+                    tmpList = []
+                    for k in range(word.senseStart, word.senseStart + word.senseNum):
+                        for j in range(w.senseStart, w.senseStart + w.senseNum):
+                            tmpList.append([k, j])
+
+                    distances, reversedDistances, minIdx, differences = sess.run([reversedDistance, distance, minLossIdx, difference], feed_dict={sensePlaceholder: tmpList})
+
+                    for k in range(len(distances)):
+                        rf.write('%s\t%s\t%.4f\t%.4f\n' % (w.token, word.token, distances[k], reversedDistances[k]))
 
     print('Data size:', len(data))
+    print('Encountered data size:', len(data))
